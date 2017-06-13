da1a0851f203bd7d8f195aa50f1df4316c31fe8d
Anant Sangar <assangar@gmail.com>
2017-06-13 20:43:57 +0100

    Setup

---
+++ 1_Basics.py
@@ -0,0 +1,40 @@
+"""
+Set the variable my_variable equal to the value 10.
+"""
+my_variable = 10
+
+"""
+Set the following variables to the corresponding values:
+my_int to the value 7
+my_float to the value 1.23
+my_bool to the value True
+"""
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+
+"""
+# Set count_to equal to the sum of two big numbers
+"""
+
+count_to = 1010 + 12
+print("count_to:",count_to)
+
+"""
+#Set eggs equal to 100 using exponentiation on line 3!
+"""
+eggs = 100 ** 2
+print("eggs:", eggs)
+
+"""
+Write a single-line comment on line 1. It can be anything! (Make sure it starts with #)
+Set the variable monty equal to True.
+Set another variable python equal to 1.234.
+Set a third variable monty_python equal to python squared.
+"""
+
+monty = True
+python = 1.234
+monty_python = python ** 2
+print("monty_python:", monty_python)
