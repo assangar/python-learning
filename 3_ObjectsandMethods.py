a2a44863d929e3bd99cd7ad0ba53a26328669814
Anant Sangar <assangar@gmail.com>
2017-06-25 15:19:54 +0200

    ObjectsandMethods

---
+++ 3_ObjectsandMethods.py
@@ -0,0 +1,10 @@
+
+
+"""
+Introduction into Object Oriented Programming
+
+In Python we store information in Objects. We also have methods which are called on objects.
+"""
+
+user_input = input("Please enter your name")
+print("Your name is:", user_input)
