be0349a399b45476d998cc5f4a6afe21ac447e8e
Anant Sangar <assangar@gmail.com>
2017-07-03 20:24:20 +0100

    User_input

--- 3_ObjectsandMethods.py
+++ 3_ObjectsandMethods.py
@@ -6,5 +6,6 @@ Introduction into Object Oriented Programming
 In Python we store information in Objects. We also have methods which are called on objects.
 """
 
-user_input = input("Please enter your name")
-print("Your name is:", user_input)
+user_input = input("Please enter your first name")
+user_input2 = input("Please enter your last name")
+print("Your name is:",user_input+" "+user_input2)
