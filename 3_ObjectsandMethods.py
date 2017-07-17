3bc0064a07bbe044e183c85f42ad98357f7929ef
Anant Sangar <assangar@gmail.com>
2017-07-17 20:45:39 +0100

    Exercise

--- 3_ObjectsandMethods.py
+++ 3_ObjectsandMethods.py
@@ -12,3 +12,12 @@ print("Your name is:",user_input+" "+user_input2)
 """
 This is a test
 """
+
+
+"""
+Exercises:
+Write a script which takes a string from a user then takes the first letter and prints it in capital
+"""
+
+user_data = input("Please enter your word")
+print(user_data[0].upper())
