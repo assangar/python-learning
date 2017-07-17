3635dab2f1ab9e104d5ddd0a07b3c5118b968139
Anant Sangar <anantsangar@selenium.offlon>
2017-07-17 10:37:33 +0100

    Testing Git

--- 3_ObjectsandMethods.py
+++ 3_ObjectsandMethods.py
@@ -9,3 +9,6 @@ In Python we store information in Objects. We also have methods which are called
 user_input = input("Please enter your first name")
 user_input2 = input("Please enter your last name")
 print("Your name is:",user_input+" "+user_input2)
+"""
+This is a test
+"""
